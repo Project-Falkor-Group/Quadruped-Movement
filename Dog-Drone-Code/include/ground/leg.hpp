1e0928b1e5f39ad479453077f824e959e8bbd9a1
Adrien Carrou <dridriswim@gmail.com>
2022-06-06 22:10:38 -0700

    adds stubbed angle to pulse function for 12 motors

--- Dog-Drone-Code/include/ground/leg.hpp
+++ Dog-Drone-Code/include/ground/leg.hpp
@@ -8,5 +8,7 @@ namespace DogDrone::Walk
         Leg()
         {
         }
+
+        
     };
 }
--- Dog-Drone-Code/include/ground/router.hpp
+++ Dog-Drone-Code/include/ground/router.hpp
@@ -1,12 +1,74 @@
 #pragma once
 
-namespace DogDrone::Walk    
+namespace DogDrone::Walk
 {
     class Router
     {
     public:
         Router()
         {
+        }
+
+        int AngleToPulse(int angle, int motor_number)
+        {
+            double pulse;
+
+            //Front Right Leg
+            if (motor_number == 0)
+            {
+                pulse = angle;
+            }
+            else if (motor_number == 1)
+            {
+                pulse = angle;
+            }
+            else if (motor_number == 2)
+            {
+                pulse = angle;
+            }
+
+            //Front Left Leg
+            else if (motor_number == 3)
+            {
+                pulse = angle;
+            }
+            else if (motor_number == 4)
+            {
+                pulse = angle;
+            }
+            else if (motor_number == 5)
+            {
+                pulse = angle;
+            }
+
+            //Back Right Leg
+            else if (motor_number == 6)
+            {
+                pulse = angle;
+            }
+            else if (motor_number == 7)
+            {
+                pulse = angle;
+            }
+            else if (motor_number == 8)
+            {
+                pulse = angle;
+            }
+
+            //Back Left Leg
+            else if (motor_number == 9)
+            {
+                pulse = angle;
+            }
+            else if (motor_number == 10)
+            {
+                pulse = angle;
+            }
+            else if (motor_number == 11)
+            {
+                pulse = angle;
+            }
+            return pulse;
         }
     };
 }
